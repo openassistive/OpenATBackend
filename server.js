fe99076e977182234d8d486419b6b63edc274b8d
will wade <willwade@gmail.com>
2016-11-05 13:24:18 +0000

    updated for heroku port

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 
 require('./routes')(app);
-app.listen('8081')
-console.log('Magic happens on port 8081');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 exports = module.exports = app;
