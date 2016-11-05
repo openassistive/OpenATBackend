24a12bb7d18dbcc71dd9c7a2b84ab998b9893565
will wade <willwade@gmail.com>
2016-11-05 13:30:02 +0000

    added mkdir stuff

--- server.js
+++ server.js
@@ -2,6 +2,25 @@ var express = require('express');
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
+// Init stuff
+var mkdirp = require('mkdirp');
+
+mkdirp('download_image', function (err) {
+    if (err) console.error(err)
+    else console.log('Could not make download_image dir')
+});
+
+mkdirp('md', function (err) {
+    if (err) console.error(err)
+    else console.log('Could not make md dir')
+});
+
+mkdirp('json', function (err) {
+    if (err) console.error(err)
+    else console.log('Could not make json dir')
+});
+
+
 require('./routes')(app);
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
