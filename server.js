80cfa4ddeb3a63cc6db5ff5f2efdc8afa42a7a77
will wade <willwade@gmail.com>
2016-11-19 21:42:07 +0000

    addition of cors

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+  , cors = require('cors')
+  , app = express();
+app.use(cors());
+
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 app.set('port', (process.env.PORT || 5000));
@@ -18,7 +21,26 @@ mkdirp('output', function (err) {
 });
 
 
-require('./routes')(app);
+// Routes
+
+module.exports = function(app) {
+
+    // Insert routes
+    app.use('/add/pinshape', cors(), require('./js/pinshape'));
+    app.use('/add/github', cors(), require('./js/github'));
+    app.use('/add/instructables', cors(), require('./js/instructables'));
+    app.use('/add/thingiverse', cors(), require('./js/thingiverse'));
+    app.use('/add/sourceforge', cors(), require('./js/sourceforge'));
+    app.use('/save', cors(), require('./js/savejson'));
+    app.route('/*')
+        .get(function(req, res) {
+            res.json({ error: "Sorry, this site is not supported" });
+        });
+};
+
+
+// Main
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
